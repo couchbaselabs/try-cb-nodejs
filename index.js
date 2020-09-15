e51aedd5ba1fb673ebfdd8989e2d70f59cf963a8
Brett Lawson <brett19@gmail.com>
2020-09-15 09:29:58 -0700

    Updated to use modern programming practices.
    Also fixed some bugs in the hotel search implementation.

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ var jwt = require('jsonwebtoken');
 var JWT_KEY = 'IAMSOSECRETIVE!';
 
 // Create a Couchbase Cluster connection
-var cluster = new couchbase.Cluster('couchbase://localhost', {
-  username: 'Administrator',
-  password: 'password'
-});
+var cluster = new couchbase.Cluster(
+  'couchbase://localhost',
+  {
+    username: 'Administrator',
+    password: 'password'
+  }
+);
 
 // Open a specific Couchbase bucket, `travel-sample` in this case.
 var bucket = cluster.bucket('travel-sample');
@@ -30,14 +33,14 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 function authUser(req, res, next) {
-  bearerToken()(req, res, function() {
+  bearerToken()(req, res, () => {
     // Temporary Hack to extract the token from the request
-    req.token = req.headers.authentication.split(" ")[1]
-    jwt.verify(req.token, JWT_KEY, function(err, decoded) {
+    req.token = req.headers.authentication.split(' ')[1];
+    jwt.verify(req.token, JWT_KEY, (err, decoded) => {
       if (err) {
         res.status(400).send({
-            error: 'Invalid JWT token',
-            cause: err
+          error: 'Invalid JWT token',
+          cause: err,
         });
         return;
       }
@@ -48,20 +51,33 @@ function authUser(req, res, next) {
   });
 }
 
-app.get('/', function (req, res) {
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(500).send({
+      error: err,
+    });
+    return;
+  }
+
+  next();
+});
+
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/api/airports', async function(req, res) {
-  var searchTerm = req.query.search;
+app.get('/api/airports', async (req, res) => {
+  const searchTerm = req.query.search;
 
-  var qs;
+  let qs;
   if (searchTerm.length === 3) {
     // FAA code
     qs = `SELECT airportname from \`travel-sample\` WHERE faa = '${searchTerm.toUpperCase()}';`;
-  } else if (searchTerm.length === 4 &&
-      (searchTerm.toUpperCase() === searchTerm ||
-        searchTerm.toLowerCase() === searchTerm)) {
+  } else if (
+    searchTerm.length === 4 &&
+    (searchTerm.toUpperCase() === searchTerm ||
+      searchTerm.toLowerCase() === searchTerm)
+  ) {
     // ICAO code
     qs = `SELECT airportname from \`travel-sample\` WHERE icao = '${searchTerm.toUpperCase()}';`;
   } else {
@@ -69,344 +85,281 @@ app.get('/api/airports', async function(req, res) {
     qs = `SELECT airportname from \`travel-sample\` WHERE LOWER(airportname) LIKE '%${searchTerm.toLowerCase()}%';`;
   }
 
-  let result, rows;
-  try {
-    result = await cluster.query(qs);
-    rows = result.rows;
-  } catch (error) {
-    console.error(error)
-  };
+  const result = await cluster.query(qs);
+  const rows = result.rows;
 
   res.send({
     data: rows,
-    context: [qs]
+    context: [qs],
   });
 });
 
-app.get('/api/flightPaths/:from/:to', async function(req, res) {
-  var fromAirport = req.params.from;
-  var toAirport = req.params.to;
-  var leaveDate = new Date(req.query.leave);
+app.get('/api/flightPaths/:from/:to', async (req, res) => {
+  const fromAirport = req.params.from;
+  const toAirport = req.params.to;
+  const leaveDate = new Date(req.query.leave);
 
-  var dayOfWeek = leaveDate.getDay();
+  const dayOfWeek = leaveDate.getDay();
 
-  var qs1 =`
-    SELECT faa AS fromAirport FROM \`travel-sample\` WHERE airportname = '${fromAirport}' 
-    UNION 
-    SELECT faa AS toAirport FROM \`travel-sample\` WHERE airportname = '${toAirport}';
-  `;
+  let qs1 = `
+      SELECT faa AS fromAirport FROM \`travel-sample\` WHERE airportname = '${fromAirport}' 
+      UNION 
+      SELECT faa AS toAirport FROM \`travel-sample\` WHERE airportname = '${toAirport}';
+    `;
 
-  let result, rows;
-  try {
-    result = await cluster.query(qs1)
-    rows = result.rows;
-  } catch (error) {
-    console.error(error)
-  };
-
+  const result = await cluster.query(qs1);
+  const rows = result.rows;
   if (rows.length !== 2) {
     res.status(404).send({
-      error: "One of the specified airports is invalid.",
-      context: [qs1]
+      error: 'One of the specified airports is invalid.',
+      context: [qs1],
     });
     return;
   }
 
-  var fromFaa = rows[0].fromAirport || rows[1].fromAirport;
-  var toFaa = rows[0].toAirport || rows[1].toAirport;
+  const fromFaa = rows[0].fromAirport || rows[1].fromAirport;
+  const toFaa = rows[0].toAirport || rows[1].toAirport;
 
-  var qs2 =`
-    SELECT a.name, s.flight, s.utc, r.sourceairport, r.destinationairport, r.equipment 
-    FROM \`travel-sample\` AS r UNNEST r.schedule AS s 
-    JOIN \`travel-sample\` AS a ON KEYS r.airlineid 
-    WHERE r.sourceairport = '${fromFaa}' AND r.destinationairport = '${toFaa}' AND s.day = ${dayOfWeek} 
-    ORDER BY a.name ASC;
-  `;
+  let qs2 = `
+      SELECT a.name, s.flight, s.utc, r.sourceairport, r.destinationairport, r.equipment 
+      FROM \`travel-sample\` AS r UNNEST r.schedule AS s 
+      JOIN \`travel-sample\` AS a ON KEYS r.airlineid 
+      WHERE r.sourceairport = '${fromFaa}' AND r.destinationairport = '${toFaa}' AND s.day = ${dayOfWeek} 
+      ORDER BY a.name ASC;
+    `;
 
-  try {
-    result = await cluster.query(qs2);
-  } catch (error) {
-    console.error(error)
-  };
-
-  if (result.rows.length === 0) {
+  const result2 = await cluster.query(qs2);
+  const rows2 = result2.rows;
+  if (rows2.length === 0) {
     res.status(404).send({
-      error: "No flights exist between these airports.",
-      context: [qs1, qs2]
+      error: 'No flights exist between these airports.',
+      context: [qs1, qs2],
     });
     return;
   }
 
-  rows = result.rows
-
-  for (var i = 0; i < rows.length; ++i) {
-    rows[i].flighttime = Math.ceil(Math.random() * 8000);
-    rows[i].price = Math.ceil(rows[i].flighttime / 8 * 100) / 100;
-  }
+  rows.forEach((row) => {
+    row.flighttime = Math.ceil(Math.random() * 8000);
+    row.price = Math.ceil((row.flighttime / 8) * 100) / 100;
+  });
 
   res.send({
     data: rows,
-    context: [qs1, qs2]
+    context: [qs1, qs2],
   });
 });
 
-app.post('/api/user/login', function(req, res) {
-  var user = req.body.user;
-  var password = req.body.password;
+app.post('/api/user/login', async (req, res) => {
+  const user = req.body.user;
+  const password = req.body.password;
 
-  var userDocKey = 'user::' + user;
+  try {
+    const userDocKey = 'user::' + user;
+    const result = await coll.get(userDocKey);
 
-  coll.get(userDocKey, function(err, doc) {
-    if (err) {
-      if (err === couchbase.errors.KEY_ENOENT) {
-        res.status(401).send({
-          error: 'User does not exist.'
-        });
-        return;
-      }
-
-      res.status(500).send({
-        error: err
-      });
-      return;
-    }
-
-    if (doc.value.password !== password) {
+    if (result.value.password !== password) {
       res.status(401).send({
-        error: 'Password does not match.'
+        error: 'Password does not match.',
       });
       return;
     }
 
-    var token = jwt.sign({
-      user: user
-    }, JWT_KEY);
-
-    res.send({
-      data: {
-        token: token
-      }
-    });
-  });
-});
-
-app.post('/api/user/signup', function(req, res) {
-  var user = req.body.user;
-  var password = req.body.password;
-
-  var userDocKey = 'user::' + user;
-  var userDoc = {
-    name: user,
-    password: password,
-    flights: []
-  };
-
-  coll.insert(userDocKey, userDoc, function(err, doc) {
-    if (err) {
-      if (err === couchbase.errors.KEY_EEXISTS) {
-        res.status(409).send({
-          error: 'User already exists.'
-        });
-        return;
-      }
-
-      res.status(500).send({
-        error: err
-      });
-      return;
-    }
-
-    var token = jwt.sign({
-      user: user
-    }, JWT_KEY);
-
-    res.send({
-      data: {
-        token: token
+    const token = jwt.sign(
+      {
+        user: user,
       },
-      context: ['created document ' + userDocKey]
+      JWT_KEY
+    );
+
+    res.send({
+      data: {
+        token: token,
+      },
     });
-  });
-});
-
-app.get('/api/user/:username/flights', authUser, function(req, res) {
-  var username = req.params.username;
-
-  if (username !== req.user.user) {
-    res.status(401).send({
-      error: 'Username does not match token username.'
-    });
-    return;
-  }
-
-  var userDocKey = 'user::' + username;
-  coll.get(userDocKey, function(err, doc) {
-    if (err) {
-      if (err == couchbase.errors.KEY_ENOENT) {
-        res.status(403).send({
-          error: 'Could not find user.'
-        });
-        return;
-      }
-
-      res.status(500).send({
-        error: err
+  } catch (err) {
+    if (err instanceof couchbase.errors.DocumentNotFoundError) {
+      res.status(401).send({
+        error: 'User does not exist.',
       });
       return;
     }
 
-    if (!doc.value.flights) {
-      doc.value.flights = [];
+    throw err;
+  }
+});
+
+app.post('/api/user/signup', async (req, res) => {
+  const user = req.body.user;
+  const password = req.body.password;
+
+  try {
+    const userDocKey = 'user::' + user;
+    const userDoc = {
+      name: user,
+      password: password,
+      flights: [],
+    };
+
+    await coll.insert(userDocKey, userDoc);
+
+    const token = jwt.sign(
+      {
+        user: user,
+      },
+      JWT_KEY
+    );
+
+    res.send({
+      data: {
+        token: token,
+      },
+      context: ['created document ' + userDocKey],
+    });
+  } catch (err) {
+    if (err instanceof couchbase.errors.DocumentExistsError) {
+      res.status(409).send({
+        error: 'User already exists.',
+      });
+      return;
+    }
+
+    throw err;
+  }
+});
+
+app.get('/api/user/:username/flights', authUser, async (req, res) => {
+  const username = req.params.username;
+
+  if (username !== req.user.user) {
+    res.status(401).send({
+      error: 'Username does not match token username.',
+    });
+    return;
+  }
+
+  try {
+    const userDocKey = 'user::' + username;
+    const result = await coll.get(userDocKey);
+
+    if (!result.content.flights) {
+      result.content.flights = [];
     }
 
     res.send({
-      data: doc.value.flights
-    })
-  });
-});
-
-app.post('/api/user/:username/flights', authUser, function(req, res) {
-  var username = req.params.username;
-  var flights = req.body.flights;
-
-  if (username !== req.user.user) {
-    res.status(401).send({
-      error: 'Username does not match token username.'
+      data: result.content.flights,
     });
-    return;
-  }
-
-  var userDocKey = 'user::' + username;
-  coll.get(userDocKey, function(err, doc) {
-    if (err) {
-      if (err == couchbase.errors.KEY_ENOENT) {
-        res.status(403).send({
-          error: 'Could not find user.'
-        });
-        return;
-      }
-
-      res.status(500).send({
-        error: err
+  } catch (err) {
+    if (err instanceof couchbase.errors.DocumentNotFoundError) {
+      res.status(403).send({
+        error: 'Could not find user.',
       });
       return;
     }
 
-    if (!doc.value.flights) {
-      doc.value.flights = [];
-    }
-
-    doc.value.flights  = doc.value.flights.concat(flights);
-
-    coll.replace(userDocKey, doc.value, {cas:doc.cas}, function(err, result) {
-      if (err) {
-        res.status(500).send({
-          error: err
-        });
-        return;
-      }
-
-      res.send({
-        data: {
-          added: flights
-        },
-        context: 'updated document ' + userDocKey
-      })
-    });
-  });
+    throw err;
+  }
 });
 
-app.get('/api/hotel/:description/:location?', async function(req, res) {
-  var description = req.params.description;
-  var location = req.params.location;
+app.post('/api/user/:username/flights', authUser, async (req, res) => {
+  const username = req.params.username;
+  const flights = req.body.flights;
 
-  var qp = couchbase.SearchQuery.conjuncts([couchbase.SearchQuery.term('hotel').field('type')]);
+  if (username !== req.user.user) {
+    res.status(401).send({
+      error: 'Username does not match token username.',
+    });
+    return;
+  }
+
+  try {
+    const userDocKey = 'user::' + username;
+    const result = await coll.get(userDocKey);
+
+    if (!result.content.flights) {
+      result.content.flights = [];
+    }
+
+    result.content.flights = result.content.flights.concat(flights);
+
+    await coll.replace(userDocKey, result.content, { cas: result.cas });
+
+    res.send({
+      data: {
+        added: flights,
+      },
+      context: 'updated document ' + userDocKey,
+    });
+  } catch (err) {
+    if (err instanceof couchbase.errors.DocumentNotFoundError) {
+      res.status(403).send({
+        error: 'Could not find user.',
+      });
+      return;
+    }
+
+    throw err;
+  }
+});
+
+app.get('/api/hotel/:description/:location?', async (req, res) => {
+  const description = req.params.description;
+  const location = req.params.location;
+  const qp = couchbase.SearchQuery.conjuncts([
+    couchbase.SearchQuery.term('hotel').field('type'),
+  ]);
 
   if (location && location !== '*') {
-    qp.and(couchbase.SearchQuery.disjuncts(
-        couchbase.SearchQuery.match(location).field("country"),
-        couchbase.SearchQuery.match(location).field("city"),
-        couchbase.SearchQuery.match(location).field("state"),
-        couchbase.SearchQuery.match(location).field("address")
-    ));
+    qp.and(
+      couchbase.SearchQuery.disjuncts(
+        couchbase.SearchQuery.match(location).field('country'),
+        couchbase.SearchQuery.match(location).field('city'),
+        couchbase.SearchQuery.match(location).field('state'),
+        couchbase.SearchQuery.match(location).field('address')
+      )
+    );
   }
-
   if (description && description !== '*') {
     qp.and(
-        couchbase.SearchQuery.disjuncts(
-            couchbase.SearchQuery.match(description).field("description"),
-            couchbase.SearchQuery.match(description).field("name")
-        ));
+      couchbase.SearchQuery.disjuncts(
+        couchbase.SearchQuery.match(description).field('description'),
+        couchbase.SearchQuery.match(description).field('name')
+      )
+    );
   }
 
-  var q = couchbase.SearchQuery.new('hotels', qp)
-      .limit(100);
+  const result = await cluster.searchQuery('hotels', qp, { limit: 100 });
+  const rows = result.rows;
+  if (rows.length === 0) {
+    res.send({
+      data: [],
+      context: [],
+    });
+    return;
+  }
 
-  await cluster.searchQuery(q, function(err, rows) {
-    if (err) {
-      res.status(500).send({
-        error: err
+  const results = await Promise.all(
+    rows.map(async (row) => {
+      const doc = await coll.get(row.id, {
+        project: [
+          'type',
+          'country',
+          'city',
+          'state',
+          'address',
+          'name',
+          'description',
+        ],
       });
-      return;
-    }
+      return doc.content;
+    })
+  );
 
-    if (rows.length === 0) {
-      res.send({
-        data: [],
-        context: []
-      });
-      return;
-    }
-
-    var results = [];
-
-    var totalHandled = 0;
-    for (var i = 0; i < rows.length; ++i) {
-      (function(row) {
-        coll.lookupIn(row.id)
-            .get('country')
-            .get('city')
-            .get('state')
-            .get('address')
-            .get('name')
-            .get('description')
-            .execute(function (err, docFrag) {
-              if (totalHandled === -1) {
-                return;
-              }
-
-              var doc = {};
-
-              try {
-                doc.country = docFrag.content('country');
-                doc.city = docFrag.content('city');
-                doc.state = docFrag.content('state');
-                doc.address = docFrag.content('address');
-                doc.name = docFrag.content('name');
-              } catch (e) { }
-
-              // This is in a separate block since some versions of the
-              //  travel-sample data set do not contain a description.
-              try {
-                doc.description = docFrag.content('description');
-              } catch (e) { }
-
-              results.push(doc);
-
-              totalHandled++;
-
-              if (totalHandled >= rows.length) {
-                res.send({
-                  data: results,
-                  context: []
-                });
-              }
-            });
-      })(rows[i]);
-    }
+  res.send({
+    data: results,
+    context: [],
   });
 });
 
-app.listen(8080, function () {
+app.listen(8080, () => {
   console.log('Example app listening on port 8080!');
 });
