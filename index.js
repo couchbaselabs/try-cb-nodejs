4b618ba565fb362caa9170a78fe39d5c2aa04bbe
Hakim Cassimally <hakim.cassimally@couchbase.com>
2021-05-25 08:59:57 +0100

    DOC-8536 fix error handling
     * move error handler to end (after all routes and use) so that it runs
     * however Async routes don't trigger handler by default.
       Use a `runAsync` wrapper to make this work.
        * NB: https://github.com/vkarpov15/awaitjs-express/ might be a good
          solution for more worked example
        * indenting all async handlers means lots of *whitespace* changes,
          view diff with `-w`
     * remove vestigial try/catch, and 500 handlers, where the global handler
       is now being used.
     * improve output
        * add comment for the INVALID_PACKET error (mostly for our benefit,
          as devs in this period where we're hitting it, but can imagine
          it will affect a few users in due course too)
     * make it clear when `res.send()` is the exit point of function, by

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var bearerToken = require('express-bearer-token');
 var cors = require('cors');
 var couchbase = require('couchbase');
+
 var express = require('express');
 var jwt = require('jsonwebtoken');
 var morgan = require('morgan')
@@ -36,11 +37,10 @@ function authUser(req, res, next) {
     req.token = req.headers.authorization.split(' ')[1];
     jwt.verify(req.token, JWT_KEY, (err, decoded) => {
       if (err) {
-        res.status(400).send({
+        return res.status(400).send({
           error: 'Invalid JWT token',
           cause: err,
         });
-        return;
       }
 
       req.user = decoded;
@@ -49,16 +49,8 @@ function authUser(req, res, next) {
   });
 }
 
-app.use((err, req, res, next) => {
-  if (err) {
-    res.status(500).send({
-      error: err,
-    });
-    return;
-  }
 
-  next();
-});
+
 
 /*
 Python reference app gives:
@@ -97,354 +89,367 @@ app.get('/', (req, res) => {
 
   const shared = app._router.stack.flatMap(format(''))
   const tenanted = tenants.stack.flatMap(format('/api/tenants/:tenant'))
-  res.send(
+  return res.send(
     JSON.stringify( shared.concat(tenanted) )
   )
 })
 
-app.get('/api/airports', async (req, res) => {
-  const searchTerm = req.query.search;
-
-  let where;
-  if (searchTerm.length === 3) {
-    // FAA code
-    where = `faa = '${searchTerm.toUpperCase()}';`;
-  } else if (
-    searchTerm.length === 4 &&
-    (searchTerm.toUpperCase() === searchTerm ||
-      searchTerm.toLowerCase() === searchTerm)
-  ) {
-    // ICAO code
-    where = `icao = '${searchTerm.toUpperCase()}';`;
-  } else {
-    // Airport name
-    where = `LOWER(airportname) LIKE '%${searchTerm.toLowerCase()}%';`;
+function runAsync (callback) {
+  return function (req, res, next) {
+    callback(req, res, next)
+      .catch(next)
   }
+}
 
-  let qs = `SELECT airportname from \`travel-sample\`.inventory.airport WHERE ${ where }`;
+app.get('/api/airports',
+  runAsync(async (req, res) => {
+    const searchTerm = req.query.search;
+    let where;
+    if (searchTerm.length === 3) {
+      // FAA code
+      where = `faa = '${searchTerm.toUpperCase()}';`;
+    } else if (
+      searchTerm.length === 4 &&
+      (searchTerm.toUpperCase() === searchTerm ||
+        searchTerm.toLowerCase() === searchTerm)
+    ) {
+      // ICAO code
+      where = `icao = '${searchTerm.toUpperCase()}';`;
+    } else {
+      // Airport name
+      where = `LOWER(airportname) LIKE '%${searchTerm.toLowerCase()}%';`;
+    }
 
-  try {
+    let qs = `SELECT airportname from \`travel-sample\`.inventory.airport WHERE ${ where }`;
+
     const result = await cluster.query(qs);
     const data = result.rows;
     const context = [`N1QL query - scoped to inventory: ${qs}`]
-    res.send({data, context})
-  }
-  catch (err) {
-    res.status(500).send({
-      error: err
-    });
-    return;
-  }
-});
+    return res.send({data, context})
+  })
+)
 
-app.get('/api/flightPaths/:from/:to', async (req, res) => {
-  const fromAirport = req.params.from;
-  const toAirport = req.params.to;
-  const leaveDate = new Date(req.query.leave);
+app.get('/api/flightPaths/:from/:to',
+  runAsync(async (req, res) => {
+    const fromAirport = req.params.from;
+    const toAirport = req.params.to;
+    const leaveDate = new Date(req.query.leave);
 
-  const dayOfWeek = leaveDate.getDay();
+    const dayOfWeek = leaveDate.getDay();
 
-  let qs1 = `SELECT faa AS fromAirport FROM \`travel-sample\`.inventory.airport
-            WHERE airportname = '${fromAirport}'
-            UNION
-            SELECT faa AS toAirport FROM \`travel-sample\`.inventory.airport
-            WHERE airportname = '${toAirport}';`;
+    let qs1 = `SELECT faa AS fromFaa
+              FROM \`travel-sample\`.inventory.airport
+              WHERE airportname = '${fromAirport}'
+              UNION
+              SELECT faa AS toFaa
+              FROM \`travel-sample\`.inventory.airport
+              WHERE airportname = '${toAirport}';`;
 
-  const result = await cluster.query(qs1);
-  const rows = result.rows;
-  if (rows.length !== 2) {
-    res.status(404).send({
-      error: 'One of the specified airports is invalid.',
-      context: [qs1],
-    });
-    return;
-  }
-  const airports = { ...rows[0], ...rows[1] }
-  const fromFaa = airports.fromAirport;
-  const toFaa   = airports.toAirport;
+    const result = await cluster.query(qs1);
+    const rows = result.rows;
+    if (rows.length !== 2) {
+      return res.status(404).send({
+        error: 'One of the specified airports is invalid.',
+        context: [qs1],
+      });
+    }
+    const { fromFaa, toFaa } = { ...rows[0], ...rows[1] }
 
-  let qs2 = `
-      SELECT a.name, s.flight, s.utc, r.sourceairport, r.destinationairport, r.equipment
-      FROM \`travel-sample\`.inventory.route AS r
-      UNNEST r.schedule AS s
-      JOIN \`travel-sample\`.inventory.airline AS a ON KEYS r.airlineid
-      WHERE r.sourceairport = '${fromFaa}'
-      AND r.destinationairport = '${toFaa}'
-      AND s.day = ${dayOfWeek}
-      ORDER BY a.name ASC;
-    `;
+    let qs2 = `
+        SELECT a.name, s.flight, s.utc, r.sourceairport, r.destinationairport, r.equipment
+        FROM \`travel-sample\`.inventory.route AS r
+        UNNEST r.schedule AS s
+        JOIN \`travel-sample\`.inventory.airline AS a ON KEYS r.airlineid
+        WHERE r.sourceairport = '${fromFaa}'
+        AND r.destinationairport = '${toFaa}'
+        AND s.day = ${dayOfWeek}
+        ORDER BY a.name ASC;
+      `;
 
-  const result2 = await cluster.query(qs2);
-  const rows2 = result2.rows;
-  if (rows2.length === 0) {
-    res.status(404).send({
-      error: 'No flights exist between these airports.',
-      context: [qs1, qs2],
-    });
-    return;
-  }
+    const result2 = await cluster.query(qs2);
+    const rows2 = result2.rows;
+    if (rows2.length === 0) {
+      return res.status(404).send({
+        error: 'No flights exist between these airports.',
+        context: [qs1, qs2],
+      })
+    }
 
-  rows2.forEach((row) => {
-    row.flighttime = Math.ceil(Math.random() * 8000);
-    row.price = Math.ceil((row.flighttime / 8) * 100) / 100;
-  });
+    rows2.forEach((row) => {
+      row.flighttime = Math.ceil(Math.random() * 8000);
+      row.price = Math.ceil((row.flighttime / 8) * 100) / 100;
+    })
 
-  res.send({
-    data: rows2,
-    context: ["N1QL query - scoped to inventory: ", qs2],
-  });
-});
-
-
+    return res.send({
+      data: rows2,
+      context: ["N1QL query - scoped to inventory: ", qs2],
+    })
+  })
+)
 
 app.use('/api/tenants/:tenant/', tenants)
 
-function makeKey(key) {
-  return key.toLowerCase()
-}
+const makeKey = key => key.toLowerCase()
 
-tenants.route('/user/login').post(async (req, res) => {
-  const tenant = makeKey( req.params.tenant )
-  const user = makeKey( req.body.user );
-  const password = req.body.password;
-  var scope = bucket.scope(tenant)
-  var users = scope.collection("users")
+tenants.route('/user/login').post(
+  runAsync(async (req, res) => {
+    const tenant = makeKey( req.params.tenant )
+    const user = makeKey( req.body.user );
+    const password = req.body.password;
+    var scope = bucket.scope(tenant)
+    var users = scope.collection("users")
 
-  try {
-    const result = await users.get(user);
+    try {
+      const result = await users.get(user);
 
-    if (result.value.password !== password) {
-      res.status(401).send({
-        error: 'Password does not match.',
+      if (result.value.password !== password) {
+        return res.status(401).send({
+          error: 'Password does not match.',
+        });
+      }
+
+      const token = jwt.sign({user}, JWT_KEY);
+
+      return res.send({
+        data: {token},
+        context: [`KV get - scoped to ${tenant}.users: for password field in document ${user}`]
       });
-      return;
+    } catch (err) {
+      if (err instanceof couchbase.DocumentNotFoundError) {
+        return res.status(401).send({
+          error: 'User does not exist.',
+        });
+      }
+      else {
+        throw(err)
+      }
     }
+  })
+)
 
-    const token = jwt.sign({user}, JWT_KEY);
+tenants.route('/user/signup').post(
+  runAsync(async (req, res) => {
+    const user = req.body.user
+    const userDocKey = makeKey(user)
+    const password = req.body.password
 
-    res.send({
-      data: {token},
-      context: [`KV get - scoped to ${tenant}.users: for password field in document ${user}`]
-    });
-  } catch (err) {
-    if (err instanceof couchbase.DocumentNotFoundError) {
-      res.status(401).send({
-        error: 'User does not exist.',
+    const tenant = makeKey( req.params.tenant )
+    var scope = bucket.scope(tenant)
+    var users = scope.collection("users")
+
+    try {
+      const userDoc = {
+        name: user,
+        password: password,
+        flights: [],
+      };
+
+      await users.insert(userDocKey, userDoc);
+
+      const token = jwt.sign({user}, JWT_KEY);
+
+      return res.send({
+        data: {token},
+        context: [`KV insert - scoped to ${tenant}.users: document ${userDocKey}`]
       });
-      return;
+    } catch (err) {
+      if (err instanceof couchbase.DocumentExistsError) {
+        return res.status(409).send({
+          error: 'User already exists.',
+        });
+      }
+      else {
+        throw(err)
+      }
     }
-
-    res.status(500).send({error: err})
-  }
-});
-
-tenants.route('/user/signup').post(async (req, res) => {
-  console.log("RARR!")
-  const user = req.body.user
-  const userDocKey = makeKey(user)
-  const password = req.body.password
-
-  const tenant = makeKey( req.params.tenant )
-  var scope = bucket.scope(tenant)
-  var users = scope.collection("users")
-
-  try {
-    const userDoc = {
-      name: user,
-      password: password,
-      flights: [],
-    };
-
-    await users.insert(userDocKey, userDoc);
-
-    const token = jwt.sign({user}, JWT_KEY);
-
-    res.send({
-      data: {token},
-      context: [`KV insert - scoped to ${tenant}.users: document ${userDocKey}`]
-    });
-  } catch (err) {
-    if (err instanceof couchbase.DocumentExistsError) {
-      res.status(409).send({
-        error: 'User already exists.',
-      });
-      return;
-    }
-
-    console.log(err)
-    res.status(500).send({error: err})
-  }
-});
+  })
+)
 
 tenants.route('/user/:username/flights')
-.get(authUser, async (req, res) => {
-  const username = req.params.username
-  const userDocKey = makeKey(username)
+.get(authUser,
+  runAsync(async (req, res) => {
+    const username = req.params.username
+    const userDocKey = makeKey(username)
 
-  const tenant = makeKey( req.params.tenant )
-  var scope = bucket.scope(tenant)
-  var users = scope.collection("users")
-  var bookings = scope.collection("bookings")
+    const tenant = makeKey( req.params.tenant )
+    var scope = bucket.scope(tenant)
+    var users = scope.collection("users")
+    var bookings = scope.collection("bookings")
 
-  if (username !== req.user.user) {
-    res.status(401).send({
-      error: 'Username does not match token username.',
-    });
-    return;
-  }
-
-  try {
-    const result = await users.get(userDocKey);
-    const ids = result.content.bookings || []
-
-    const inflated = await Promise.all(
-      ids.map(
-        async flightId => (await bookings.get(flightId)).content))
-
-    res.send({
-      data: inflated,
-      context: `KV get - scoped to ${tenant}.user: for ${ids.length} bookings in document ${userDocKey}`,
-    })
-
-  } catch (err) {
-    if (err instanceof couchbase.DocumentNotFoundError) {
-      res.status(403).send({
-        error: 'Could not find user.',
+    if (username !== req.user.user) {
+      return res.status(401).send({
+        error: 'Username does not match token username.',
       });
-      return;
     }
 
-    res.status(500).send({error: err})
-  }
-})
-.put(authUser, async (req, res) => {
-  const username = req.params.username
-  const userDocKey = makeKey(username)
+    try {
+      const result = await users.get(userDocKey);
+      const ids = result.content.bookings || []
 
-  const newFlight = req.body.flights[0]
-  const tenant = makeKey( req.params.tenant )
+      const inflated = await Promise.all(
+        ids.map(
+          async flightId => (await bookings.get(flightId)).content))
 
-  var scope = bucket.scope(tenant)
-  var users = scope.collection("users")
-  var bookings = scope.collection("bookings")
+      return res.send({
+        data: inflated,
+        context: `KV get - scoped to ${tenant}.user: for ${ids.length} bookings in document ${userDocKey}`,
+      })
+    } catch (err) {
+      if (err instanceof couchbase.DocumentNotFoundError) {
+        return res.status(403).send({
+          error: 'Could not find user.',
+        })
+      }
+      else {
+        throw(err)
+      }
+    }
+  })
+)
+.put(authUser,
+  runAsync(async (req, res) => {
+    const username = req.params.username
+    const userDocKey = makeKey(username)
 
-  if (username !== req.user.user) {
-    res.status(401).send({
-      error: 'Username does not match token username.',
-    });
-    return;
-  }
+    const newFlight = req.body.flights[0]
+    const tenant = makeKey( req.params.tenant )
 
-  const flightId = uuid.v4()
+    var scope = bucket.scope(tenant)
+    var users = scope.collection("users")
+    var bookings = scope.collection("bookings")
 
-  try {
-    await bookings.upsert(flightId, newFlight)
-  }
-  catch (err) {
-    res.status(500).send({
-      error: 'Failed to add flight data',
-    });
-    return;
-  }
-
-  try {
-    await users.mutateIn(userDocKey, [
-      couchbase.MutateInSpec.arrayAppend(
-        'bookings',
-        flightId,
-        { createPath: true })])
-
-    res.send({
-      data: {
-        added: newFlight,
-      },
-      context:
-        [`KV mutateIn - scoped to ${tenant}.users: for bookings subdocument field in document ${userDocKey}`]
-    })
-    return
-  } catch (err) {
-    if (err instanceof couchbase.DocumentNotFoundError) {
-      res.status(403).send({
-        error: 'Could not find user.',
+    if (username !== req.user.user) {
+      return res.status(401).send({
+        error: 'Username does not match token username.',
       });
-      return;
     }
 
-    res.status(500).send({error: err})
-  }
-});
+    const flightId = uuid.v4()
 
-app.get('/api/hotels/:description/:location?', async (req, res) => {
-  const description = req.params.description;
-  const location = req.params.location;
-  var scope = bucket.scope("inventory")
-  var hotels = scope.collection("hotel")
-  const qp = couchbase.SearchQuery.conjuncts([
-    couchbase.SearchQuery.term('hotel').field('type'),
-  ]);
-
-  if (location && location !== '*') {
-    qp.and(
-      couchbase.SearchQuery.disjuncts(
-        couchbase.SearchQuery.match(location).field('country'),
-        couchbase.SearchQuery.match(location).field('city'),
-        couchbase.SearchQuery.match(location).field('state'),
-        couchbase.SearchQuery.match(location).field('address')
-      )
-    );
-  }
-  if (description && description !== '*') {
-    qp.and(
-      couchbase.SearchQuery.disjuncts(
-        couchbase.SearchQuery.match(description).field('description'),
-        couchbase.SearchQuery.match(description).field('name')
-      )
-    );
-  }
-
-  const result = await cluster.searchQuery('hotels-index', qp, { limit: 100 });
-  const rows = result.rows;
-  if (rows.length === 0) {
-    res.send({
-      data: [],
-      context: [`FTS search - scoped to: inventory.hotel (no results)\n${JSON.stringify(qp)}`],
-    });
-    return;
-  }
-
-  const addressCols = [
-    'address',
-    'state',
-    'city',
-    'country'
-  ]
-
-  const cols = [
-    'type',
-    'name',
-    'description',
-    ...addressCols
-  ]
-
-  const results = await Promise.all(
-    rows.map(async (row) => {
-      const doc = await hotels.get(row.id, {
-        project: cols
+    try {
+      await bookings.upsert(flightId, newFlight)
+    }
+    catch (err) {
+      return res.status(500).send({
+        error: 'Failed to add flight data',
       });
-      var content = doc.content
-      content.address =
-        addressCols
-        .flatMap(field => content[field] || [])
-        .join(', ')
-      return content;
-    })
-  );
+    }
 
-  res.send({
-    data: results,
-    context: [
-      `FTS search - scoped to: inventory.hotel within fields ${cols.join(', ')}\n${JSON.stringify(qp)}`]
-  });
+    try {
+      await users.mutateIn(userDocKey, [
+        couchbase.MutateInSpec.arrayAppend(
+          'bookings',
+          flightId,
+          { createPath: true })])
+
+      return res.send({
+        data: {
+          added: newFlight,
+        },
+        context:
+          [`KV mutateIn - scoped to ${tenant}.users: for bookings subdocument field in document ${userDocKey}`]
+      })
+    } catch (err) {
+      if (err instanceof couchbase.DocumentNotFoundError) {
+        return res.status(403).send({
+          error: 'Could not find user.',
+        });
+      }
+      else {
+        throw(err)
+      }
+    }
+  })
+)
+
+app.get('/api/hotels/:description/:location?',
+  runAsync(async (req, res) => {
+    const description = req.params.description;
+    const location = req.params.location;
+    var scope = bucket.scope("inventory")
+    var hotels = scope.collection("hotel")
+    const qp = couchbase.SearchQuery.conjuncts([
+      couchbase.SearchQuery.term('hotel').field('type'),
+    ]);
+
+    if (location && location !== '*') {
+      qp.and(
+        couchbase.SearchQuery.disjuncts(
+          couchbase.SearchQuery.match(location).field('country'),
+          couchbase.SearchQuery.match(location).field('city'),
+          couchbase.SearchQuery.match(location).field('state'),
+          couchbase.SearchQuery.match(location).field('address')
+        )
+      );
+    }
+    if (description && description !== '*') {
+      qp.and(
+        couchbase.SearchQuery.disjuncts(
+          couchbase.SearchQuery.match(description).field('description'),
+          couchbase.SearchQuery.match(description).field('name')
+        )
+      );
+    }
+
+    const result = await cluster.searchQuery('hotels-index', qp, { limit: 100 });
+    const rows = result.rows;
+    if (rows.length === 0) {
+      return res.send({
+        data: [],
+        context: [`FTS search - scoped to: inventory.hotel (no results)\n${JSON.stringify(qp)}`],
+      });
+    }
+
+    const addressCols = [
+      'address',
+      'state',
+      'city',
+      'country'
+    ]
+
+    const cols = [
+      'type',
+      'name',
+      'description',
+      ...addressCols
+    ]
+
+    const results = await Promise.all(
+      rows.map(async (row) => {
+        const doc = await hotels.get(row.id, {
+          project: cols
+        });
+        var content = doc.content
+        content.address =
+          addressCols
+          .flatMap(field => content[field] || [])
+          .join(', ')
+        return content;
+      })
+    )
+
+    return res.send({
+      data: results,
+      context: [
+        `FTS search - scoped to: inventory.hotel within fields ${cols.join(', ')}\n${JSON.stringify(qp)}`]
+    })
+  })
+)
+
+// Error handler. Must be defined after other routes/middleware
+app.use((err, req, res, next) => {
+  const errText = err.toString()
+  if (errText.match(/LCB_ERR_KVENGINE_INVALID_PACKET/)) {
+    return res.status(500).send({
+      error: "Received LCB_ERR_KVENGINE_INVALID_PACKET error from Couchbase. Please check the SDK release notes and ensure you are using a compatible server version."
+    })
+  }
+  else {
+    return res.status(500).send({
+      error: `${err.toString()}: ${JSON.stringify(err)}`
+    });
+  }
+  next()
 });
 
 app.listen(8080, () => {
